e35a4c108c9a066aca10d952c8604feeb1d27d61
jeff_makuto <68159268+jeffmakuto@users.noreply.github.com>
2024-11-04 16:30:36 +0300

    Update settings.py

--- faq_bot/faq_bot/settings.py
+++ faq_bot/faq_bot/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'localhost',              # For local development
     '127.0.0.1',              # Localhost IP
-    'your-backend-name.onrender.com',  # Render URL for backend
+    'https://bot-hbu6.onrender.com',  # Render URL for backend
 ]
 
 # Application definition
